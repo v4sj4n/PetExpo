9046665af103d7931fed75d38499df672b8ffd9f
Vasjan Çupri <vasjancupri04@gmail.com>
2024-05-17 15:59:58 +0200

    Implemented fetching and will style the page better now

--- src/pages/PetCategory.tsx
+++ src/pages/PetCategory.tsx
@@ -1,9 +1,58 @@
 import React from "react";
-import { useParams, useOutletContext } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function PetCategory() {
+  const [animalArray, setAnimalArray] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
   const { petCategory } = useParams();
-  console.log(petCategory)
+  useEffect(() => {
+    if (
+      petCategory === "dogs" ||
+      petCategory === "cats" ||
+      petCategory === "birds"
+    ) {
+      const fetchPetCategory = async () => {
+        try {
+          const res = await fetch(
+            `https://freetestapi.com/api/v1/${petCategory}`
+          );
+          console.log(res);
+          const data = await res.json();
+          setAnimalArray(data);
+          setLoading(false);
+        } catch (err) {
+          setError("Error fetching data");
+          setLoading(false);
+        }
+      };
+      fetchPetCategory();
+    } else {
+      setLoading(false);
+      setError("Invalid Category");
+    }
+  }, [petCategory]);
 
-  return <div>PetCategory</div>;
+  console.log(petCategory);
+
+  if (loading) {
+    return <div className="text-white">Loading...</div>;
+  }
+  if (error) {
+    return <div className="text-white">{error}</div>;
+  }
+  return (
+    <div>
+      <h1>{petCategory}</h1>
+      <ul>
+        {animalArray.map((animal: any) => (
+          <li key={animal.id} className="text-white">
+            {animal.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
